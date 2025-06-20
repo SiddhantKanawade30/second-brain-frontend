85bc6b1609903c93465e47c8f62b42a38e2ab54d
Siddhant Kanawade <kanawadesiddhant30@gmail.com>
2025-06-21 02:24:50 +0530

    button ui

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,30 @@
-export interface ButtonProps {
-    variant : "primary" | "secondary";
+import type { ReactElement } from "react";
+
+
+type variants = "primary" | "secondary" ;
+
+
+interface ButtonProps {
+    variant : variants;
     size : "sm" | "md" | "lg";
     text : string;
-    startIcon : any ;
-    endIcon : any;
-    onClick : ()=> void 
+    startIcon?: ReactElement;
+    endIcon?: ReactElement;
+    onClick : ()=> void ;
 }
 
-export const Button = (props : ButtonProps) =>{
-    return <button />
+
+const variantStyles = {
+    "primary" : "bg-violet-600 text-white ",
+    "secondary" : "bg-violet-300 text-violet-700"
 }
+
+
+
+export const Button = (props : ButtonProps) =>{
+
+    return <button className={variantStyles[props.variant]}>{props.text}</button>
+
+}
+
+{/* <Button variant="primary" size="sm" text="asas" onClick={()=>{}} /> */}
