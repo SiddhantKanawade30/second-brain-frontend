c98d3206bd36a0e052b8e31cffa193134ee79a90
Siddhant Kanawade <kanawadesiddhant30@gmail.com>
2025-06-24 20:12:24 +0530

    card component top bar

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,17 @@
 import { Share } from "../icons/Share"
 import { PlusIcon } from "../icons/Plus"
+import { Delete } from "../icons/delete"
 
 export function Card() {
     return (
-        <div className="">
-
+        <div className="flex w-[300px] h-[200px] border p-4 justify-between items-center bg-gray-200">
+            <div>
+                <span className="text-black w-8 h-8 inline-block"><PlusIcon /></span>
+            </div>
+            <div className="flex gap-2">
+                <span className="text-black w-8 h-8 inline-block"><Share /></span>
+                <span className="text-black w-8 h-8 inline-block"><Delete /></span>
+            </div>
         </div>
     );
 }
